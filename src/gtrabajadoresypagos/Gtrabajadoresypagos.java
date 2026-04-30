208863009c14ab3f55a60fc72e1704ed99b531b7
Jiro13 <saenzhuaripataj@gmail.com>
2026-04-29 21:26:11 -0500

    configuramos opcion 1

--- src/gtrabajadoresypagos/Gtrabajadoresypagos.java
+++ src/gtrabajadoresypagos/Gtrabajadoresypagos.java
@@ -19,6 +19,7 @@ public class Gtrabajadoresypagos {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         ArrayList<Trabajador> lista = new ArrayList<>();
+        Trabajador t = new Trabajador();
 
         int opcion;
 
@@ -32,7 +33,40 @@ public class Gtrabajadoresypagos {
             opcion = sc.nextInt();
             sc.nextLine(); 
             if (opcion==1){
-                System.out.println("Ingrese el nombre: ");
+                 System.out.print("Nombre: ");
+    t.setNombre(sc.nextLine());
+
+    System.out.print("Apellido paterno: ");
+    t.setApellido_paterno(sc.nextLine());
+
+    System.out.print("Apellido materno: ");
+    t.setApellidos_materno(sc.nextLine());
+
+    System.out.print("Tipo doc (DNI/Residencia): ");
+    t.setTipo_doc(sc.nextLine());
+
+    System.out.print("Numero doc: ");
+    t.setNum_doc(sc.nextLine());
+
+    System.out.print("Regimen (728/Locacion): ");
+    t.setRegimen(sc.nextLine());
+
+    System.out.print("Tipo pension (Integra/Prima/Habitat/ONP): ");
+    t.setTipo_pension(sc.nextLine());
+
+    System.out.print("Sueldo base: ");
+    t.setSueldo_base(sc.nextDouble());
+
+    System.out.print("Tiene hijos (si/no): ");
+    t.setTiene_hijos(sc.nextBoolean());
+
+    System.out.print("Trabaja de noche (si/no): ");
+    t.setTrabaja_noche(sc.nextBoolean());
+    sc.nextLine();
+
+    lista.add(t);
+
+    System.out.println("Trabajador agregado correctamente");
                 
                 
             }
