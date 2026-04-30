e2d54b60d167548ec14eac4e5c73b010e9646a3a
Jiro13 <saenzhuaripataj@gmail.com>
2026-04-29 20:52:36 -0500

    encapsulamiento

--- src/Trabajador.java
+++ src/Trabajador.java
@@ -18,6 +18,86 @@ public class Trabajador {
    private double sueldo_base;
    private boolean tiene_hijos;
    private boolean trabaja_noche;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido_paterno() {
+        return apellido_paterno;
+    }
+
+    public void setApellido_paterno(String apellido_paterno) {
+        this.apellido_paterno = apellido_paterno;
+    }
+
+    public String getApellidos_materno() {
+        return apellidos_materno;
+    }
+
+    public void setApellidos_materno(String apellidos_materno) {
+        this.apellidos_materno = apellidos_materno;
+    }
+
+    public String getTipo_doc() {
+        return tipo_doc;
+    }
+
+    public void setTipo_doc(String tipo_doc) {
+        this.tipo_doc = tipo_doc;
+    }
+
+    public String getNum_doc() {
+        return num_doc;
+    }
+
+    public void setNum_doc(String num_doc) {
+        this.num_doc = num_doc;
+    }
+
+    public String getRegimen() {
+        return regimen;
+    }
+
+    public void setRegimen(String regimen) {
+        this.regimen = regimen;
+    }
+
+    public String getTipo_pension() {
+        return tipo_pension;
+    }
+
+    public void setTipo_pension(String tipo_pension) {
+        this.tipo_pension = tipo_pension;
+    }
+
+    public double getSueldo_base() {
+        return sueldo_base;
+    }
+
+    public void setSueldo_base(double sueldo_base) {
+        this.sueldo_base = sueldo_base;
+    }
+
+    public boolean isTiene_hijos() {
+        return tiene_hijos;
+    }
+
+    public void setTiene_hijos(boolean tiene_hijos) {
+        this.tiene_hijos = tiene_hijos;
+    }
+
+    public boolean isTrabaja_noche() {
+        return trabaja_noche;
+    }
+
+    public void setTrabaja_noche(boolean trabaja_noche) {
+        this.trabaja_noche = trabaja_noche;
+    }
     
     
 }
