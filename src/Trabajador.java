9830842c863c474ddbafbed34886f3493c560144
Jiro13 <saenzhuaripataj@gmail.com>
2026-04-29 20:49:53 -0500

    agregamos atributos

--- src/Trabajador.java
+++ src/Trabajador.java
@@ -8,7 +8,16 @@
  * @author LENOVO
  */
 public class Trabajador {
-   
+   private String nombre;
+   private String apellido_paterno;
+   private String apellidos_materno;
+   private String tipo_doc;
+   private String num_doc;
+   private String regimen;
+   private String tipo_pension;
+   private double sueldo_base;
+   private boolean tiene_hijos;
+   private boolean trabaja_noche;
     
     
 }
