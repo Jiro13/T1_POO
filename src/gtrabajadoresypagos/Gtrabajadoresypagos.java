0f957eaa86b0260e0f821ef8236e84e22cb9dca9
Jiro13 <saenzhuaripataj@gmail.com>
2026-04-29 20:59:41 -0500

    creamos el menu y agregamos scanner

--- src/gtrabajadoresypagos/Gtrabajadoresypagos.java
+++ src/gtrabajadoresypagos/Gtrabajadoresypagos.java
@@ -4,6 +4,9 @@
  */
 package gtrabajadoresypagos;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  *
  * @author LENOVO
@@ -14,7 +17,21 @@ public class Gtrabajadoresypagos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+
+        int opcion;
+
+        do {
+            System.out.println(" MENU");
+            System.out.println("1. Agregar trabajador");
+            System.out.println("2. Listar trabajadores");
+            System.out.println("3. Calcular sueldos");
+            System.out.println("4. Salir");
+            System.out.print("Opcion: ");
+            opcion = sc.nextInt();
+            sc.nextLine(); // limpiar buffer
+        }while (opcion != 4);
     }
-    
+
 }
